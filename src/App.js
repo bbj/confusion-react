5d5e7f638ad25a4178d874b7bb86cf53684e5740
Bruno Jullien <bruno.jullien@gmail.com>
2021-03-26 12:09:01 -0700

    configuring react and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    )
+  }
+} 
+export default App;
+
+/** Generated code
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +44,4 @@ function App() {
 }
 
 export default App;
+ */
