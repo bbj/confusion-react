db750accb163d0c51c7e4e066362cff957ac453d
Bruno Jullien <bruno.jullien@gmail.com>
2021-03-31 16:31:02 -0700

    components part 2 completed

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-//import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            selectedDish: null
         }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });    //MUST USE setState()!!!!
+    }
+
+    renderDish(dish) {
+        if (dish === null) {
+            return (
+                <div></div>
+            );
+         } else {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+         }
     }
 
     render() {
@@ -18,16 +40,13 @@ class Menu extends Component {
          */
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={ () => this.onDishSelect(dish) }>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
@@ -35,9 +54,10 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
